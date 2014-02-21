c45b0e6c3c140a85b2d3f1eee272f69ffe5c6ea2
Christopher O'Brien <obriencj@gmail.com>
2014-02-20 19:36:42 -0500

    test stub, setuptools stub

--- setup.py
+++ setup.py
@@ -1,9 +1,57 @@
-from distutils.core import setup
+#! /usr/bin/env python3
 
 
-# todo: we need a unittest command in distutils
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, see
+# <http://www.gnu.org/licenses/>.
+
+
+"""
+Sibilant, Scheme for Python
+
+author: Christopher O'Brien  <obriencj@gmail.com>
+license: LGPL v.3
+"""
+
+
+from setuptools import setup
+
+
+setup( name = "sibilant",
+       version = "0.9.0",
+
+       packages = [ "sibilant", ],
+
+       test_suite = "tests",
+
+       # PyPI information
+       author = "Christopher O'Brien",
+       author_email = "obriencj@gmail.com",
+       url = "https://github.com/obriencj/python-sibilant",
+       license = "GNU Lesser General Public License",
+
+       description = "Scheme modules for Python"
+
+       provides = [ "sibilant", ]
+       requires = [],
+       platforms = [ "python3 >= 3.3", ],
+
+       zip_safe = False,
+
+       classifiers = [ "Intended Audience :: Developers",
+                       "Programming Language :: Python :: 3",
+                       "Topic :: Software Development", ], )
 
 
 #
 # The end.
-
