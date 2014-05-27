90bc932366d00d6a1cdca450d2e60258d4466e75
Christopher O'Brien <obriencj@gmail.com>
2014-05-26 21:34:54 -0400

    not even sure this is wise

--- sibilant/builtins.py
+++ sibilant/builtins.py
@@ -21,46 +21,43 @@ license: LGPL v.3
 """
 
 
+import operator
 from functools import reduce
 
 
-# we can replace the class supplying number functions here
-numbers = float
+def _number_op_k(opf):
+    fun = lambda k, *a: k(reduce(opf, a))
+    fun.__name__ = opf.__name__
+    fun.__doc__ = opf.__doc__
+    return fun
 
 
-def number_op_k(opf):
-    # creates a k call conforming number operand function
-    opf =
-    return lambda k, *a: k(reduce(opf, a))
+add_k = _number_op_k(numbers.__add__)
+sub_k = _number_op_k(numbers.__sub__)
+mult_k = _number_op_k(numbers.__mult__)
+divide_k = _number_op_k(numbers.__div__)
+mod_k = _number_op_k(numbers.__mod__)
+pow_k = _number_op_k(numbers.__pow__)
 
 
-add_k = number_op_k(numbers.__add__)
-sub_k = number_op_k(numbers.__sub__)
-mult_k = number_op_k(numbers.__mult__)
-divide_k = number_op_k(numbers.__div__)
-mod_k = number_op_k(numbers.__mod__)
-pow_k = number_op_k(numbers.__pow__)
+def cons_k(k, item, l):
+    return k(cons(item, l))
 
 
-def cons_k(k, item, list):
-    return k(cons(item, list))
+def car_k(k, l):
+    return k(car(l))
 
 
-def car_k(k, list):
-    return k(list.car)
-
-
-def cdr_k(k, list):
-    return k(list.cdr)
+def cdr_k(k, l):
+    return k(cdr(l))
 
 
 def list_k(k, *a):
     return k(reduce(lambda x,y: cons(y,x), a[::-1], null))
 
 
-class symbol(object):
-    def __init__(self, s):
-        self.sym = s
+def apply_k(k, f, *a):
+    return f(k, *a)
 
 
 #
