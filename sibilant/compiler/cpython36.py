ce5d2319a8b986ebd2115a184d6dc0682b14c1de
Christopher O'Brien <obriencj@gmail.com>
2017-07-29 23:57:28 -0400

    headerific

--- sibilant/compiler/cpython36.py
+++ sibilant/compiler/cpython36.py
@@ -1,3 +1,16 @@
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, see
+# <http://www.gnu.org/licenses/>.
 
 
 from . import (
@@ -65,7 +78,7 @@ class CPython36(SpecialsCodeSpace):
         return b''.join(bytes(c) for c in coll)
 
 
-    def gen_code(self, declare_label, declare_position):
+    def gen_opcode(self, declare_label, declare_position):
         """
         Given the pseudo operations added to this CodeSpace, the named
         variables declared and requested, yield the CPython opcodes
