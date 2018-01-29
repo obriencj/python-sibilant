ed027f50be15345dd14fd1a01b286fce0bcb61e8
Christopher O'Brien <obriencj@gmail.com>
2018-01-29 14:49:40 -0500

    move some functions from bootstrap to basics

--- sibilant/bootstrap.py
+++ sibilant/bootstrap.py
@@ -53,8 +53,6 @@ def __setup__(glbls):
     import sibilant.specials as specials
     import sibilant.operators as operators
 
-    from sibilant import is_pair, TypePredicate
-
 
     _all_ = []
 
@@ -77,7 +75,7 @@ def __setup__(glbls):
 
     def _ty(type_, name):
         _val(type_, name)
-        _op(TypePredicate(name + "?", type_), None, True)
+        _op(sibilant.TypePredicate(name + "?", type_), None, True)
 
 
     # === mass re-export from other modules ==
@@ -156,110 +154,9 @@ def __setup__(glbls):
     _op(copy, "copy")
     _op(deepcopy, "deep-copy")
 
-
-    def _as_tuple(value):
-        """
-        (to-tuple VALUE)
-
-        Converts VALUE to a tuple. If VALUE is a cons pair, will
-        unpack it. Otherwise, VALUE will be iterated over and its
-        contents collected.
-        """
-
-        if is_pair(value):
-            return tuple(value.unpack())
-        else:
-            return tuple(value)
-
-    _op(_as_tuple, "to-tuple", rename=True)
-
-
-    def _as_list(value):
-        """
-        (to-list VALUE)
-
-        Converts VALUE to a list. If VALUE is a cons pair, will
-        unpack it. Otherwise, VALUE will be iterated over and its
-        contents collected.
-        """
-
-        if is_pair(value):
-            return list(value.unpack())
-        else:
-            return list(value)
-
-    _op(_as_list, "to-list", rename=True)
-
-
-    def _as_set(value):
-        """
-        (to-set VALUE)
-
-        Converts VALUE to a set. If VALUE is a cons pair, will
-        unpack it. Otherwise, VALUE will be iterated over and its
-        contents collected.
-        """
-
-        if is_pair(value):
-            return set(value.unpack())
-        else:
-            return set(value)
-
-    _op(_as_set, "to-set", rename=True)
-
-
-    def _count(value):
-        """
-        (count VALUE)
-
-        The number of items in VALUE. If VALUE is a cons pair, will
-        unpack it. Otherwise, identical to (len VALUE)
-        """
-
-        return value.count() if is_pair(value) else len(value)
-
-    _op(_count, "count", rename=True)
-
-
-    def apply(fun, arglist=(), kwargs={}):
-        """
-        (apply FUN)
-        (apply FUN arglist: POSITIONALS)
-        (apply FUN kwargs: KEYWORDS)
-        (apply FUN arglist: POSITIONALS kwargs: KEYWORDS)
-
-        Invokes FUN as a function, with optional iterable
-        POSITIONALS as positional arguments, and optional mapping
-        KEYWORDS as keyword arguments.
-
-        If POSITIONALS is a a cons pair, it will be unpacked instead of
-        iterated
-        """
-
-        # this doesn't need to be an operator or a special, because in
-        # instances where this could be compiled inline, someone could
-        # just as easily write (FUN *: POSITIONALS **: KEYWORDS) and
-        # have it be inlined. The only real use of apply is as a
-        # runtime function, passed along to call its arguments.
-
-        if is_pair(arglist):
-            arglist = arglist.unpack()
-        return fun(*arglist, **kwargs)
-
-    _op(apply, "apply", rename=True)
-
-
-    # _op(sibilant.build_tuple, "values")
-    # _op(sibilant.build_tuple, "build-tuple")
     _ty(tuple, "tuple")
-
-    # _op(sibilant.build_list, "build-list")
     _ty(list, "list")
-
-    # _op(sibilant.build_dict, "build-dict")
     _ty(dict, "dict")
-
-    # _op(sibilant.build_set, "build-set")
     _ty(set, "set")
 
     _op(lambda value: hasattr(value, "__iter__"),
@@ -271,8 +168,9 @@ def __setup__(glbls):
     _ty(map, "map")
     _ty(zip, "zip")
     _ty(filter, "filter")
-    _op(reduce, "reduce")
     _ty(enumerate, "enumerate")
+
+    _op(reduce, "reduce")
 
     _op(callable, "callable?")
     _op(next, "next")
@@ -295,6 +193,7 @@ def __setup__(glbls):
     _ty(fraction, "fraction")
     _ty(range, "range")
     _ty(slice, "slice")
+
     _op(chr, "chr")
     _op(ord, "ord")
     _op(min, "min")
