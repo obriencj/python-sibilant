2f310bebb8b69c443fc41c8b0f9a9d9bfc33e54c
Christopher O'Brien <obriencj@gmail.com>
2018-01-30 09:34:12 -0500

    more venv features for ease-of-development

--- venv.sh
+++ venv.sh
@@ -4,41 +4,77 @@
 VENV="$HOME/.virtualenvs"
 BRANCH=$(git symbolic-ref --short -q HEAD)
 
+VDIR="$VENV/sibilant-$BRANCH"
+VBIN="$VDIR/bin"
 
-VENV_BRANCH="$VENV/sibilant-$BRANCH"
-VPY="$VENV_BRANCH/bin/python"
-VSIB="$VENV_BRANCH/bin/sibilant"
+CMD="$1"
+shift
 
 
-if test "$1" == "help" ; then
+SYSPYTHON=$(readlink -f $(which python3))
+
+
+if test "$CMD" == "help" || test -z "$CMD" ; then
     cat <<EOF
-Usage: $0 [COMMAND]
+Usage: $0 COMMAND
 
-Builds and deploys into a virtualenv under $VENV_BRANCH
+Builds and deploys into a virtualenv under:
+ $VDIR
 
-COMMAND may be one of 'sibilant' or 'python' in order to immediately
-launch either the sibilant or python repl after installation.
+COMMAND may be one of the following:
+
+  help          show this message and exit
+  install       create the virtualenv and install into it
+  sibilant      run sibilant from the virtualenv
+  python        run python from the virtualenv
+  pip           run pip from the virtualenv
+  sys-python    run system python with PYTHONHOME set (fix wx issues)
+  sys-python    run sibilant using system python with PYTHONHOME set
 
 EOF
-
     exit 1
-
-else
-    echo -e "Current branch is $BRANCH so deploying into:\n $VENV_BRANCH"
-    mkdir -p "$VENV_BRANCH"
-
-    { virtualenv "$VENV_BRANCH" && "$VPY" setup.py clean build install ; } \
-	|| exit $?
-
-    if test "$1" == "sibilant" ; then
-	$VSIB
-    elif test "$1" == "python" ; then
-	$VPY
-    elif test -n "$1" ; then
-	echo -e "\nUnknown command: $1"
-	exit 1
-    fi
 fi
+
+
+echo -e "Current branch is $BRANCH so working in:\n $VDIR"
+
+case "$CMD" in
+    install)
+	mkdir -p "$VDIR"
+
+	virtualenv --python="$SYSPYTHON" "$@" "$VDIR" || exit $?
+	"$VBIN/python" setup.py clean build install || exit $?
+
+	;;
+
+    sibilant)
+	"$VBIN/sibilant" "$@"
+	;;
+
+    python)
+	"$VBIN/python" "$@"
+	;;
+
+    pip)
+	"$VBIN/pip" "$@"
+	;;
+
+    sys-python)
+	# wxPython doesn't like virtualenv very much on macOS. You
+	# have to run the system version, and get it to operate inside
+	# the venv by setting the PYTHONHOME parameter.
+	PYTHONHOME="$VDIR" "$SYSPYTHON" "$@"
+	;;
+
+    sys-sibilant)
+	PYTHONHOME="$VDIR" "$SYSPYTHON" "$VBIN/sibilant" "$@"
+	;;
+
+    *)
+	echo -e "\nUnknown command: $CMD"
+	exit 1
+	;;
+esac
 
 
 #
