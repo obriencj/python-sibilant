563843c67d2d8662058f5eee55ca5c36d1acbb75
Christopher O'Brien <obriencj@gmail.com>
2020-03-01 10:41:56 -0500

    simplify our docker-compose case for locally testing under multiple python versions

--- tests/docker/tests.sh
+++ tests/docker/tests.sh
@@ -1,13 +1,26 @@
 #! /bin/bash
 
-TESTNAME="$1"
-LOGFILE=/src/logs/"$TESTNAME"-test.log
 
-cd /src
-mkdir -p logs
+function py3_impl_ver() {
+    python3 -c 'import sys; print(sys.implementation.cache_tag);'
+}
 
-echo "Results for $TESTNAME" > "$LOGFILE"
-python3 -u setup.py test flake8 >>"$LOGFILE" 2>&1
+
+function run_tests() {
+    local TESTNAME=$(py3_impl_ver)
+    local LOGFILE=/src/logs/"$TESTNAME"-test.log
+
+    mkdir -p /src/logs
+    cd /src
+
+    pip3 install --upgrade pip setuptools flake8 >/dev/null
+
+    echo "Results for $TESTNAME" > "$LOGFILE"
+    python3 -u setup.py clean build test flake8 >>"$LOGFILE" 2>&1
+}
+
+
+run_tests
 
 
 # The end.
